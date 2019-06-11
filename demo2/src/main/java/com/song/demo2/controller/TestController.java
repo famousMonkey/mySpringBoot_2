794055a8af2fec7b822b002b502c14d2090b55c8
monkey <13838121279@163.com>
2019-06-11 10:40:50 +0800

    修改控制器返回类型

--- demo2/src/main/java/com/song/demo2/controller/TestController.java
+++ demo2/src/main/java/com/song/demo2/controller/TestController.java
@@ -1,19 +1,20 @@
 package com.song.demo2.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("test")
 @Slf4j
 public class TestController {
 
 
     @GetMapping(value = "/helloworld")
-    public void test(){
+    public String test(){
         log.info("=====helloWorld=====");
+        return "helloWorld";
     }
 
 }
